4210f94ea27f389a3e9c8000c2bee74a4daf9cfe
MisterNajdorf <214100015@iitb.ac.in>
2023-05-03 21:04:34 +0530

    Changes

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Heading from '../components/heading';
 
 const About = () => {
-  // About page
+  // About
   return (
     <section className="text-gray-600 body-font dark:text-gray-400 dark:bg-gray-900">
       <div className="container px-5 py-24 mx-auto">
